692bf0207100c3763c1eb8020aafb6642062ab08
MoisésHsilva1 <moihgm67@gmail.com>
2024-04-18 19:20:13 -0300

    atualizando projeto e feature

--- cypress/e2e/cucumber/Tests/RegisterTest/RegisterTest.cy.js
+++ cypress/e2e/cucumber/Tests/RegisterTest/RegisterTest.cy.js
@@ -1,10 +1,10 @@
 import {Given, When, Then, And} from "cypress-cucumber-preprocessor/steps"
 
-Given("I visit website", () => {
-       cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=account/register')
+Given('I visit website', () => {
+    cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=account/register')
 });
 
-When(" I click on the first name field", () => {
+When(' I click on the first name field', () => {
    cy.get('[name="firstname"]').eq(0).click
 });
 
@@ -12,6 +12,25 @@ And('I write my first name', () => {
     cy.get('[name="firstname"]').type('usuario teste')
 });
 
- Then("My first name will be in the field", () => {
+ Then('My first name will be in the field', () => {
     cy.get('input[name="firstname"]').should('have.value', 'usuario teste')
+});
+
+Given('I already write my first name now, I will type my last name', () => {
+
+});
+Given('I already write my first name now, I will type my last name',() => {
+    cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=account/register')
+});
+
+When('I click on the last name field', () => {
+    cy.get('#input-lastname').click()
+});
+
+And('I write my last name', () =>{
+    cy.get('[name="lastname"]').type('Sobrenome teste')
+});
+
+Then('My last name will be in the field', () =>{
+    cy.get('input[name="lastname"]').should('have.value', 'Sobrenome teste')
 });
--- cypress/e2e/cucumber/Tests/RegisterTest/RegisterTest.feature
+++ cypress/e2e/cucumber/Tests/RegisterTest/RegisterTest.feature
@@ -1,6 +1,13 @@
-Feature: Register how new user in 
-Scenario: Fill in the fields on the registration form and log in as a new user of the website
+Feature: Register how new user in website of E-commerce
+
+Scenario: Writing in the name field
    Given I visit website
    When I click on the first name field
    And I write my first name
    Then My first name will be in the field
+
+Scenario: Writing in the last name in field
+   Given I visit web site and already write my first name now, I will type my last name
+   When I click on the last name field
+   And I write my last name
+   Then My last name will be in the field
