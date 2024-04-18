df8e708731f5365b3c2676b2ba8c892e03860110
MoisésHsilva1 <moihgm67@gmail.com>
2024-04-18 18:44:05 -0300

    atualizando projeto

--- cypress/e2e/cucumber/Tests/RegisterTest/RegisterTest.cy.js
+++ cypress/e2e/cucumber/Tests/RegisterTest/RegisterTest.cy.js
@@ -1,17 +1,17 @@
 import {Given, When, Then, And} from "cypress-cucumber-preprocessor/steps"
 
 Given("I visit website", () => {
-       cy.visit('https://phptravels.org/register.php')
+       cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=account/register')
 });
 
 When(" I click on the first name field", () => {
-    cy.get('[name="firstname"]').eq(0).click()
+   cy.get('[name="firstname"]').eq(0).click
 });
 
 And('I write my first name', () => {
     cy.get('[name="firstname"]').type('usuario teste')
 });
 
-// Then("My first name will be in the field", () => {
-
-// });
+ Then("My first name will be in the field", () => {
+    cy.get('input[name="firstname"]').should('have.value', 'usuario teste')
+});
